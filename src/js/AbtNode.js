8b6d51a38f7c103cb0767b5e11fabc3c67dceccc
Kirill <75835582+r1z3rISGOD@users.noreply.github.com>
2021-04-12 11:13:24 +0300

    Update AbtNode.js

--- src/js/AbtNode.js
+++ src/js/AbtNode.js
@@ -6,9 +6,9 @@ export function AbtNode(Nodes, i) {
   Data.innerHTML += `
  <div class="AbtNodeZone">
   <h1 class="AbtNodeZone_text">
-  <img src="./Images/${Nodes[i-1][3]}.png" alt="" class="AbtNodeZone_img">
+  <img src="./Images/${Nodes[i-1][3]}.svg" alt="" class="AbtNodeZone_img">
   ${Nodes[i-1][1]}</h1>
-  <h3 class="AbtNodeZone_status">Статус<img class="AbtNodeZone_status_active" src="./Images/${Nodes[i-1][0]}.png" alt=""></h3>
+  <h3 class="AbtNodeZone_status">Статус<img class="AbtNodeZone_status_active" src="./Images/${Nodes[i-1][0]}.svg" alt=""></h3>
   <h3 class="AbtNodeZone_address">Адрес узла<input readonly value="${Nodes[i-1][4]}" class="AbtNodeZone_address_input" type="text"></h3>
   <div class="AbtNodeZone_btns">
   <button class="AbtNodeZone_more">Подробнее</button>
